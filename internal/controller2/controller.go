4ae10ec1e9dd39bf4ff1541775df7e0b70b259c7
Jaana Dogan <jbd@google.com>
2026-05-21 19:02:34 -0700

    chore: add TODO comments for execution resumption and remote harness support

--- internal/controller2/controller.go
+++ internal/controller2/controller.go
@@ -93,6 +93,9 @@ func (d *Controller) Exec(ctx context.Context, req *proto.ExecRequest, handler E
 		return fmt.Errorf("conversation_id is required")
 	}
 
+	// TODO(jbd): Resume an incomplete execution if there exists one.
+	// TODO(jbd): Enable bringing a remote harness that implements HarnessService.
+
 	h := harnesstest.NewHarness()
 	exec, err := h.Start(ctx, req.ConversationId)
 	if err != nil {
