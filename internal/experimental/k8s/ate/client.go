0aaf0d81fa7ec2625ef76b3e021255ce7bd5c0c0
Jaana Dogan <jbd@google.com>
2026-05-21 20:15:44 -0700

    Remove unnecessary ate build tags because Substrate is now public

--- internal/experimental/k8s/ate/client.go
+++ internal/experimental/k8s/ate/client.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package ate provides a client for the Agent Substrate Control API.
 package ate
 
 import (
