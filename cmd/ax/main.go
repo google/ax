aa75efaf4c0b9ce5e2ff804b327124c0c613d98d
Anjali Sridhar <anjsridhar@gmail.com>
2026-06-10 14:06:49 -0700

    Implement ax monitor command and HTTP API

--- cmd/ax/main.go
+++ cmd/ax/main.go
@@ -46,6 +46,8 @@ and run the controller server.`,
 func init() {
 	rootCmd.AddCommand(execCmd)
 	rootCmd.AddCommand(serveCmd)
+
+	rootCmd.AddCommand(monitorCmd)
 }
 
 func connect(server string) (*grpc.ClientConn, error) {
